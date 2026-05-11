de58832493b2397bba0a650d766de344d4c1ce3d
piatresmina8-web <piatresmina8@gmail.com>
2026-05-11 16:37:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 
 import os
-
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 from config import Config
 from database import db
-
-#  imports لازم في الأول
 from routes.user_routes import user_routes
 from routes.material_routes import material_routes
 from routes.price_routes import price_routes
@@ -14,42 +11,31 @@ from routes.request_routes import request_routes
 from routes.pickup_routes import pickup_routes
 from routes.company_routes import company_routes
 from routes.contact_routes import contact_routes
-
-#  مهم جدًا: استيراد كل الموديلز
 from models import material_type, material, user, company, recycle_request, pickup, contact, gift
 
-#  إنشاء التطبيق
+
 app = Flask(__name__)
 
-#  CORS
 CORS(app)
-
-#  Config
 
 app.config.from_object(Config)
 
-#  Database URL
 database_url = "sqlite:///app.db"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#  init DB
 db.init_app(app)
 
-#  إنشاء الجداول عند التشغيل
 with app.app_context():
     db.create_all()
 
-#  route لإنشاء الجداول يدوي
 @app.route("/init-db")
 
 def init_db():
     db.create_all()
     return "Database created ✅"
 
-
-#  route إضافة ماتريال
 @app.route("/add-materials", methods=["GET"])
 def add_materials():
     from models.material_type import MaterialType
@@ -79,8 +65,6 @@ image="img.png", type_id=2)
     except Exception as e:
         return {"error": str(e)}
 
-
-#  ربط routes
 user_routes(app)
 material_routes(app)
 price_routes(app)
@@ -89,14 +73,10 @@ pickup_routes(app)
 company_routes(app)
 contact_routes(app)
 
-
-# عرض الصور 
 @app.route('/uploads/<filename>')
 
 def get_image(filename):
     return send_from_directory('uploads', filename)
-
-
 
 from flask import render_template
 
@@ -105,8 +85,6 @@ def home():
     return render_template("index.html")
 
 
-
-#  تشغيل السيرفر
 port = int(os.environ.get("PORT", 8080))
 
 if __name__ == "__main__":
